a31f46c21647f222ccb9244339545b4eb6e36cd1
vance.petitt <vance.petitt@gmail.com>
2022-02-15 10:18:51 -0600

    instantiated the cars serializer and populated the cars table through Django admin

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CarSerializer
+from .models import Car
 
 @api_view(['GET'])
 def cars_list(request):
+    cars = Car.objects.all()
 
-    return Response ('ok')
+    serializer = CarSerializer(cars, many =True)
+
+    return Response(serializer.data)
